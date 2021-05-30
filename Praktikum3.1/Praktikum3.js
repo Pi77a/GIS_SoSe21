5e2a187f3b7d099e76cfbe18386a347da3c8942d
Pi77a <b3narndt@gmail.com>
2021-05-30 16:47:07 +0200

    3.1

--- Praktikum3.1/Praktikum3.js
+++ Praktikum3.1/Praktikum3.js
@@ -4,13 +4,11 @@ exports.P_3_1Server = void 0;
 const Http = require("http");
 var P_3_1Server;
 (function (P_3_1Server) {
-    let formData = new FormData(document.forms[0]);
     console.log("Starting server");
     let port = Number(process.env.PORT);
     if (!port)
         port = 8100;
     let server = Http.createServer();
-    serverrequest();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
@@ -24,17 +22,6 @@ var P_3_1Server;
         _response.write(_request.url);
         _response.end();
         console.log(_request.url);
-        for (const entry of formData) {
-            console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
-        }
-    }
-    async function serverrequest() {
-        let url = "https://grundlageninteraktivesysteme.herokuapp.com/";
-        // tslint:disable-next-line: no-any
-        await fetch(url);
-        console.log(url);
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=Praktikum3.js.map
--- Praktikum3.1/Praktikum3.ts
+++ Praktikum3.1/Praktikum3.ts
@@ -1,41 +1,28 @@
 import * as Http from "http";
 
 export namespace P_3_1Server {
-    let formData: FormData = new FormData(document.forms[0]);
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
+    console.log("Starting server"); 
+    let port: number = Number(process.env.PORT);    //eine variable namens "port" wird erstellt
+    if (!port)                                      
+        port = 8100;                                //port wird auf 8100 gesetzt. Der port wird auf lokalen Servern benutzt damit man verschiedene websites 
+                                                    //auf dem gleichen server verwenden kann mit verschiedenen ports.
 
-    let server: Http.Server = Http.createServer();
-    serverrequest();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    let server: Http.Server = Http.createServer();  //ein http Server wir erstellt und 
+    server.addListener("request", handleRequest);   //ihm werden verschiedene Listener Events hinzugefügt. Eins für das Event wenn jemand auf den Server geht
+    server.addListener("listening", handleListen);  // und eins welches sofort beim Start des Servers aktiviert wird.
     server.listen(port);
 
-    function handleListen(): void {
-        console.log("Listening");
+    function handleListen(): void {                 //dieses Event wird beim start des Servers aktiviert, und gibt daraufhin 
+        console.log("Listening");                   //den string "Listening" aus.
     }
 
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {     //wenn jemand auf den server zugreift
+        console.log("I hear voices!");                                                                 //wird erstmal der string "I hear voices!"
+        _response.setHeader("content-type", "text/html; charset=utf-8");                               //jetzt wird durch setHeader eine Html seite erstellt
+        _response.setHeader("Access-Control-Allow-Origin", "*");                                       
+        _response.write(_request.url);                                                                  
         _response.end();
         console.log(_request.url);
-        for (const entry of formData) {
-            console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
-        }
-    }
-
-    async function serverrequest(): Promise<void> {
-        let url: string = "https://grundlageninteraktivesysteme.herokuapp.com/";
-        // tslint:disable-next-line: no-any
-        await fetch(url);
-        console.log(url);
     }
 }
